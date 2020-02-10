da549836119f4c35991a5a521dc6247c939c801f
David Gomez <davdg25@gmail.com>
2020-02-10 10:14:20 -0700

    update test

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -8,7 +8,16 @@ RSpec.describe 'I can search using the Potter API' do
     click_button 'Search For Members'
 
     expect(current_path).to eq(search_path)
+    expect(page).to have_content('21 Order of the Phoenix Members in House Gryffindor')
     save_and_open_page
+    expect(page).to have_css('.member', count: 21)
+
+    within(first('.member')) do
+       expect(first('.name').text).to_not be_empty
+       expect(first('.house').text).to eq('Gryffindor')
+       expect(page).to have_css('.role')
+       expect(page).to have_css('.patronus')
+     end
 
   end
 end
