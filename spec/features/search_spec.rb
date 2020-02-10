d74736ab6130866ff3a16f4a414f5a6a7a6c9d6e
David Gomez <davdg25@gmail.com>
2020-02-10 09:53:24 -0700

    controller

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 RSpec.describe 'I can search using the Potter API' do
   it 'can search for gryffidor' do
     visit root_path
-  end
 
+    select 'Gryffindor', from: :house
+    click_button 'Search For Members'
+
+    expect(current_path).to eq(search_path)
+    save_and_open_page
+
+  end
 end
